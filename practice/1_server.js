9804138d2f1d9fc59eca7a8b8fae3b59d5d083e9
Juhyeon Lee <esloven3@gmail.com>
2025-04-25 15:21:28 +0900

    Endpoint branch processing

--- practice/1_server.js
+++ practice/1_server.js
@@ -1,5 +1,6 @@
 // import http from 'http';
 const http = require('http');
+const url = require('url');
 
 // localhost -> 127.0.0.1 -> loopback address -> local machine
 const host = "localhost";
@@ -7,9 +8,23 @@ const port = 3000;
 
 // req -> request  
 const server = http.createServer((req, res) => {
+    const path = url.parse(req.url).pathname;
+    
     // res -> response
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('<h1>Hello World</h1>');
+    if(path === '/'){
+        res.writeHead(200, {'Content-Type': 'text/html' });
+        res.end('<h1>Home Page</h1>');
+    }else if(path === '/post'){
+        res.writeHead(200, {'Content-Type': 'text/html' });
+        res.end('<h1>Post Page</h1>'); 
+    }else if(path === '/user'){
+        res.writeHead(200, {'Content-Type': 'text/html' });
+        res.end('<h1>User Page</h1>');   
+    }else{
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('<h1>404 Page Not Found!</h1>');
+    }
+
 });
 
 server.listen(port, host, () => {
