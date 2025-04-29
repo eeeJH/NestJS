5df027ceef9716e74e6a950ae559b2e2b9535983
Juhyeon Lee <esloven3@gmail.com>
2025-04-29 17:09:06 +0900

    Create Project 'SNS' by  Nest JS

---
+++ sns/src/app.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+/**
+ * author: string;
+ * title: string;
+ * content: string;
+ * likeCount: number;
+ * commentCount: number;
+ */
+
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getPost(): Post {
+    return {
+      author: 'John Doe',
+      title: 'Hello World',
+      content: 'This is a sample post content.',
+      likeCount: 10,
+      commentCount: 5,
+    };
+  }
+}
